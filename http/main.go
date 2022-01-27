764a4f9d1a27ab68b3ab0ef875e05ced59782e00
yaellec <yaelle.chaudy@grafana.com>
2022-01-27 11:14:21 +0100

    Reader Writer

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct {}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -14,5 +17,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
